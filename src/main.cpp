8a592f231d5cb4ffcf95d31e2185a2e0b0a7fd77
DevDroid42 <19912037+DevDroid42@users.noreply.github.com>
2021-05-02 17:41:41 -0500

    added note volocity calulcations
    also has a control value for the highest velocity currently pressed

--- src/main.cpp
+++ src/main.cpp
@@ -4,47 +4,80 @@
 #include <MIDIUSB.h>
 #include <note.cpp>
 
-CapacitiveSensor sensor = CapacitiveSensor(8,9);
+CapacitiveSensor sensor = CapacitiveSensor(8, 9);
+void controlChange(uint8_t channel, uint8_t controlVal, uint8_t value);
+void SelectChannel(uint8_t &channel);
+
 void setupNotes(int octave);
 const int NOTE_COUNT = 16;
 Note notes[NOTE_COUNT];
-void setup() {
-  Serial.begin(9600);       
+void setup()
+{
+  Serial.begin(115200);
   //setup multiplexer output pins
   for (size_t i = 2; i <= 5; i++)
   {
     pinMode(i, OUTPUT);
-  }    
+  }
   setupNotes(5);
 }
 
 uint8_t channel = 0;
-long input; 
-void loop() {
+//a control change of the greatest velocity is sent.
+int16_t greatestVel = 0;
+int input;
+
+void loop()
+{
+  greatestVel = -1;
   for (channel = 0; channel < NOTE_COUNT; channel++)
   {
-    digitalWrite(2, bitRead(channel, 3));
-    digitalWrite(3, bitRead(channel, 2));
-    digitalWrite(4, bitRead(channel, 1));
-    digitalWrite(5, bitRead(channel, 0));
-    input = sensor.capacitiveSensor(20);
-    if(input > 200){
-      notes[channel].noteOn(0, 255);
-    }else{
-      notes[channel].noteOff(0, 255);
+    SelectChannel(channel);
+    input = sensor.capacitiveSensor(10);
+    if (input > 65)
+    {
+      notes[channel].noteOn(0, constrain(input - 35, 0, 255));
+      //track greatest velocity
+      if (notes[channel].velocityVal > greatestVel)
+      {
+        greatestVel = notes[channel].velocityVal;
+      }
     }
+    else
+    {
+      notes[channel].noteOff(0, 0);
+    }
+
     Serial.print(input);
-    Serial.print(" ");    
+    Serial.print(" ");
   }
-  Serial.println();    
+  if (greatestVel > 0)
+  {
+    controlChange(0, 0, (uint8_t)(greatestVel * 0.3921));
+  }
+  Serial.println();
   MidiUSB.flush();
 }
 
-void setupNotes(int octave){
+void SelectChannel(uint8_t &channel)
+{
+  digitalWrite(2, bitRead(channel, 3));
+  digitalWrite(3, bitRead(channel, 2));
+  digitalWrite(4, bitRead(channel, 1));
+  digitalWrite(5, bitRead(channel, 0));
+}
+
+void setupNotes(int octave)
+{
   int start = octave * 12 + 9;
   for (size_t i = 0; i < NOTE_COUNT; i++)
   {
     notes[i].note = NOTE_COUNT - 1 - i + start;
   }
-  
+}
+
+void controlChange(uint8_t channel, uint8_t controlVal, uint8_t value)
+{
+  midiEventPacket_t event = {0x0B, 0xB0 | channel, controlVal, value};
+  MidiUSB.sendMIDI(event);
 }
--- src/note.cpp
+++ src/note.cpp
@@ -7,12 +7,20 @@
 class Note
 {
 public:
-    int note = 0;
+    uint8_t note = 0;
     bool noteActive = false;
 
-    void noteOn(byte channel, byte velocity)
+    //the live velocity value. will be different from inital note on.
+    uint8_t velocityVal = 0;
+
+    //used to track how many cycles the note has been on for.
+    uint8_t OnCycles = 0;
+
+    void noteOn(uint8_t channel, uint8_t velocity)
     {
-        if (!noteActive)
+        OnCycles++;
+        velocityVal = velocity;
+        if (!noteActive && OnCycles > (uint8_t)3)
         {
             noteActive = true;
             midiEventPacket_t noteOn = {0x09, 0x90 | channel, note, velocity};
@@ -20,22 +28,15 @@ public:
         }
     }
 
-    void noteOff(byte channel, byte velocity)
+    void noteOff(uint8_t channel, uint8_t velocity)
     {
+        OnCycles = 0;
+        velocityVal = velocity;
         if (noteActive)
         {
             noteActive = false;
             midiEventPacket_t noteOff = {0x08, 0x80 | channel, note, velocity};
             MidiUSB.sendMIDI(noteOff);
-        }
-    }
-
-    void controlChange(byte channel, byte value)
-    {
-        if (noteActive)
-        {
-            midiEventPacket_t event = {0x0B, 0xB0 | channel, note, value};
-            MidiUSB.sendMIDI(event);
         }
     }
 };
