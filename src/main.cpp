daec8ef42a7fd170aeb069c018a9e3e9c357ab8d
DevDroid42 <19912037+DevDroid42@users.noreply.github.com>
2024-05-22 18:39:57 -0500

    adapted for two Multiplexers

--- src/main.cpp
+++ src/main.cpp
@@ -2,52 +2,64 @@
 #include <CapacitiveSensor.h>
 #include <note.cpp>
 
-CapacitiveSensor sensor = CapacitiveSensor(8, 9);
-void controlChange(uint8_t channel, uint8_t controlVal, uint8_t value);
-void SelectChannel(uint8_t &channel);
+CapacitiveSensor sensorLow = CapacitiveSensor(10, 14);
+CapacitiveSensor sensorHigh = CapacitiveSensor(8, 9);
+void controlChange(const uint8_t channel, const uint8_t controlVal, const uint8_t value);
+void SelectChannel(const uint8_t channel);
 
 void setupNotes(int octave);
-const int NOTE_COUNT = 16;
+const int NOTE_COUNT = 24;
 Note notes[NOTE_COUNT];
 void setup()
 {
   Serial.begin(115200);
-  //setup multiplexer output pins
-  for (size_t i = 2; i <= 6; i++)
+  // setup multiplexer output pins
+  for (size_t i = 2; i <= 5; i++)
   {
     pinMode(i, OUTPUT);
   }
   setupNotes(4);
 }
 
-uint8_t channel = 0;
-//a control change of the greatest velocity is sent.
+// a control change of the greatest velocity is sent.
 int16_t greatestVel = 0;
 int input;
+
+void noteLogic(int input, int channel)
+{
+  if (input > 45)
+  {
+    notes[channel].noteOn(0, constrain((input - 40) * 2, 0, 255));
+    // track greatest velocity
+    if (notes[channel].velocityVal > greatestVel)
+    {
+      greatestVel = notes[channel].velocityVal;
+    }
+  }
+  else
+  {
+    notes[channel].noteOff(0, 0);
+  }
+
+  Serial.print(input);
+  Serial.print(" ");
+}
 
 void loop()
 {
   greatestVel = -1;
-  for (channel = 0; channel < NOTE_COUNT; channel++)
+  for (uint8_t channel = 0; channel < 16; channel++)
   {
     SelectChannel(channel);
-    input = sensor.capacitiveSensor(3);
-    if (input > 40)
+    if (channel < 8)
     {
-      notes[channel].noteOn(0, constrain(input - 40, 0, 255));
-      //track greatest velocity
-      if (notes[channel].velocityVal > greatestVel)
-      {
-        greatestVel = notes[channel].velocityVal;
-      }
+      input = sensorLow.capacitiveSensor(2);
+      // we have an offset of zero because these are the lower notes
+      noteLogic(input, channel + 16);
     }
-    else
-    {
-      notes[channel].noteOff(0, 0);
-    }
-
-    Serial.print(input);
-    Serial.print(" ");
+    input = sensorHigh.capacitiveSensor(2);
+    // we have an offset of 8 because these notes are 8 higher than the start
+    noteLogic(input, channel);
   }
   if (greatestVel > 0)
   {
@@ -57,13 +69,12 @@ void loop()
   MidiUSB.flush();
 }
 
-void SelectChannel(uint8_t &channel)
+void SelectChannel(const uint8_t channel)
 {
   digitalWrite(2, bitRead(channel, 3));
   digitalWrite(3, bitRead(channel, 2));
   digitalWrite(4, bitRead(channel, 1));
   digitalWrite(5, bitRead(channel, 0));
-  digitalWrite(6, bitRead(channel, 4));
 }
 
 void setupNotes(int octave)
@@ -75,7 +86,7 @@ void setupNotes(int octave)
   }
 }
 
-void controlChange(uint8_t channel, uint8_t controlVal, uint8_t value)
+void controlChange(const uint8_t channel, const uint8_t controlVal, const uint8_t value)
 {
   midiEventPacket_t event = {0x0B, 0xB0 | channel, controlVal, value};
   MidiUSB.sendMIDI(event);
